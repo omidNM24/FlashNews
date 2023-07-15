94a457075c4695f73412cfbf094c6ba9377a91a1
Omid <omidnomani24@gmail.com>
2023-07-15 18:30:24 +0430

    uninstalled redux

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,6 @@ import Navigation from "@/components/Navigation/Navigation";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import Router from "next/router";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import stateReducer from "./../store";
 import Footer from "@/components/Footer/Footer";
 
 NProgress.configure({
@@ -20,18 +17,12 @@ Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-export const store = configureStore({
-  reducer: {
-    mainState: stateReducer,
-  },
-});
-
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+    <>
       <Navigation />
       <Component {...pageProps} />
       <Footer />
-    </Provider>
+    </>
   );
 }
